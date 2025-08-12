a9fd89113eff52974470e02e840104562a744bb2
KhushiVats25 <khushivats0925@gmail.com>
2025-08-12 18:06:26 +0530

    Role entity class created

---
+++ src/main/java/system/flight/entities/Roles.java
@@ -0,0 +1,10 @@
+package system.flight.entities;
+
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+public class Roles
+{
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
