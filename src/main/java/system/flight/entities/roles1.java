83dd21260d227d845012d3d0380b8eb4f6bdd422
mythilisreesaibollina <mythilichowdarybollina@gmail.com>
2025-08-14 10:16:26 +0530

    prac tise

--- src/main/java/system/flight/entities/roles1.java
+++ src/main/java/system/flight/entities/roles1.java
@@ -4,5 +4,6 @@ public class roles1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hi");
+        System.out.println("mythili");
     }
 }
