1abb76ce63829f0120012dc6a0e6823fc30b0d6c
KhushiVats25 <khushivats0925@gmail.com>
2025-08-14 15:56:09 +0530

    Created register functionality of user

--- src/main/java/system/flight/repository/RoleRepository.java
+++ src/main/java/system/flight/repository/RoleRepository.java
@@ -1,4 +1,10 @@
 package system.flight.repository;
 
-public class RoleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import system.flight.entities.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+        boolean existsByRoleName(String roleName);
 }
