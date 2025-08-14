ef415b86c054efc16701800922d07835c2fce454
KhushiVats25 <khushivats0925@gmail.com>
2025-08-14 10:11:07 +0530

    practice

--- src/main/java/system/flight/entities/roles1.java
+++ src/main/java/system/flight/entities/roles1.java
@@ -3,5 +3,6 @@ package system.flight.entities;
 public class roles1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hi");
     }
 }
